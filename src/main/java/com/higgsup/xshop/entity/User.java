07656c0db92250686bbd090a4117ac6d5f2de1c6
vuonganhvu <anhvuong.tmdt@gmail.com>
2019-02-19 13:50:18 +0700

    fetch lazy

--- src/main/java/com/higgsup/xshop/entity/User.java
+++ src/main/java/com/higgsup/xshop/entity/User.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,7 +35,7 @@ public class User {
     @Column(name = "PASSWORD")
     private String password;
 
-    @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")
     private Role role;
 
