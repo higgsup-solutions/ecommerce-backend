d604f9eb63120cbfb60bf146a5f48e7acb9853ef
vuonganhvu <anhvuong.tmdt@gmail.com>
2019-02-21 15:47:36 +0700

    add cart

--- src/main/java/com/higgsup/xshop/dto/CartAddDTO.java
+++ src/main/java/com/higgsup/xshop/dto/CartAddDTO.java
@@ -2,10 +2,14 @@ package com.higgsup.xshop.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CartAddDTO {
 
+  @NotNull(message = "ProductId is required")
   private Integer productId;
 
+  @NotNull(message = "Amount is required")
   private Integer amount;
 }
