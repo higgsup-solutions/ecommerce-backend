4fa08d7176d46a5c307d71aa1305a385e6622ae8
tuleha <tuleha52@gmail.com>
2019-02-21 16:35:18 +0700

    remove unused import rename final war package

--- src/main/java/com/higgsup/xshop/dto/UserRegisterDTO.java
+++ src/main/java/com/higgsup/xshop/dto/UserRegisterDTO.java
@@ -3,13 +3,11 @@ package com.higgsup.xshop.dto;
 
 import com.higgsup.xshop.common.ApplicationConstant;
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
-@ConfigurationProperties(prefix = "higgsup.config.common.regex")
 public class UserRegisterDTO {
 
   private String emailRegex;
