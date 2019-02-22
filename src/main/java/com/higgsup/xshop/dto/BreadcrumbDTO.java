a610a29da8d86b72654ef95b6e22bc7f461d3a2f
Do Trung Thanh <thanhdt@higgsup.com>
2019-02-22 15:49:42 +0700

    refactor code v2

--- src/main/java/com/higgsup/xshop/dto/BreadcrumbDTO.java
+++ src/main/java/com/higgsup/xshop/dto/BreadcrumbDTO.java
@@ -1,37 +1,26 @@
 package com.higgsup.xshop.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+import java.math.BigInteger;
+
+@Data
+@RequiredArgsConstructor
+@Getter
 public class BreadcrumbDTO {
 
+  @NonNull
   private Integer id;
-
+  @NonNull
   private String name;
-
-  private Integer parentId;
-
+  @NonNull
+  private BigInteger parentId;
+  @NonNull
   private Short level;
 
-  public BreadcrumbDTO(Integer id, String name, Integer parentId,
-      Short level) {
-    this.id = id;
-    this.name = name;
-    this.parentId = parentId;
-    this.level = level;
-  }
 
-  public Integer getId() {
-    return id;
-  }
 
-  public String getName() {
-    return name;
-  }
-
-  public Integer getParentId() {
-    return parentId;
-  }
-
-  public Short getLevel() {
-    return level;
-  }
 }
