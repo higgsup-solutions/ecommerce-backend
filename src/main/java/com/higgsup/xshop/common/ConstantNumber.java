36fd7ca10c1c831d845d1d7bce17ac037799fef6
Tu Thieu Thanh <tu.thieuthanh@gmail.com>
2019-02-22 15:35:52 +0700

    move fields from ConstantPage to ConstantNumber

--- src/main/java/com/higgsup/xshop/common/ConstantNumber.java
+++ src/main/java/com/higgsup/xshop/common/ConstantNumber.java
@@ -1,6 +1,8 @@
 package com.higgsup.xshop.common;
 
 public enum ConstantNumber {
+  PAGE_INDEX_FOR_FEEDBACK(0),
+  PAGE_SIZE_FOR_FEEDBACK(5),
   NUMBER_OF_RELATED_PRODUCT (5);
 
   private Integer value;
