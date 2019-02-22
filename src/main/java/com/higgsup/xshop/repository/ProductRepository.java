3b0683e7330922406f909d9753d3bf70bf9a6c94
Tu Thieu Thanh <tu.thieuthanh@gmail.com>
2019-02-22 14:54:16 +0700

    add coding for api get related products

--- src/main/java/com/higgsup/xshop/repository/ProductRepository.java
+++ src/main/java/com/higgsup/xshop/repository/ProductRepository.java
@@ -3,8 +3,16 @@ package com.higgsup.xshop.repository;
 import com.higgsup.xshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>,
     JpaSpecificationExecutor<Product> {
 
+  @Query(value = "SELECT * from product WHERE category_id = :id "
+      + "ORDER BY RAND() LIMIT :number", nativeQuery = true)
+  List<Product> findRelatedProductByCategoryId(@Param("id") Integer id,
+      @Param("number") Integer number);
 }
