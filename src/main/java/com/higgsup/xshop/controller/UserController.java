989915386bcc41542f417309ddf2b64c732387b1
vuonganhvu <anhvuong.tmdt@gmail.com>
2019-02-28 16:33:15 +0700

    get user info

---
+++ src/main/java/com/higgsup/xshop/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.higgsup.xshop.controller;
+
+import com.higgsup.xshop.dto.UserDTO;
+import com.higgsup.xshop.dto.base.IPagedResponse;
+import com.higgsup.xshop.dto.base.ResponseMessage;
+import com.higgsup.xshop.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@Api(value = "UserController", description = "Set of methods user")
+public class UserController {
+
+  private final IUserService userService;
+
+  public UserController(IUserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/profile")
+  @ApiOperation(value = "API get current user info", response = IPagedResponse.class)
+  public IPagedResponse<UserDTO> getCurrentUserInfo() {
+    ResponseMessage<UserDTO> responseMessage = new ResponseMessage<>();
+    responseMessage.setData(userService.getCurrentUserInfo());
+    return new IPagedResponse<>(responseMessage);
+  }
+
+}
--- src/main/java/com/higgsup/xshop/service/IUserService.java
+++ src/main/java/com/higgsup/xshop/service/IUserService.java
@@ -1,5 +1,6 @@
 package com.higgsup.xshop.service;
 
+import com.higgsup.xshop.dto.UserDTO;
 import com.higgsup.xshop.entity.User;
 
 public interface IUserService {
@@ -7,5 +8,7 @@ public interface IUserService {
   User getByEmail(String email);
 
   User register(User user);
+
+  UserDTO getCurrentUserInfo();
 
 }
