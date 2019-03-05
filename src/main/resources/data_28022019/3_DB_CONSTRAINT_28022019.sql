f2e6992d458e24befd58f2b0eef57c394129412e
Thieu Thanh Tu <tu.thieuthanh@gmail.com>
2019-03-05 11:47:04 +0700

    add scrips for creating new tables

--- src/main/resources/data_28022019/3_DB_CONSTRAINT_28022019.sql
+++ src/main/resources/data_28022019/3_DB_CONSTRAINT_28022019.sql
@@ -7,6 +7,10 @@ ALTER TABLE `product` ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`supplier_id`
 ALTER TABLE `review` ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 ALTER TABLE `user` ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 ALTER TABLE `user_token` ADD CONSTRAINT `user_token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `order` ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `order` (`id`);
+ALTER TABLE `transaction` ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`xshop_order_id`) REFERENCES `order` (`id`);
+ALTER TABLE `order_detail` ADD CONSTRAINT `order_detail_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+ALTER TABLE `order_address` ADD CONSTRAINT `order_address_ibfk_1` FOREIGN KEY (`order_address`) REFERENCES `order` (`id`);
 
 SET foreign_key_checks = 1;
 
